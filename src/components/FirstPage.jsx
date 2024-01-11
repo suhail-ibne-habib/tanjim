973181085b6d7421ce90c367127bc914cb9a497a
Suhail Habib <59106269+suhail-ibne-habib@users.noreply.github.com>
2024-01-11 15:51:27 +0600

    Add files via upload

--- src/components/FirstPage.jsx
+++ src/components/FirstPage.jsx
@@ -1,22 +1,29 @@
 import Clock from "./Clock";
 import Boxes from "./Boxes";
 
+import PlaceHolder from '../assets/img/floating-img.jpg';
+
 function FirstPage() {
   return (
     <>
       <div className="page full-screen background">
-        <div className="clock-wrap sidebar colored">
-          <h2 class="clock-title">Lorem Ipsum is simply dummy<br></br> text of the printing</h2>
-          <Clock />
-        </div>
-        <div className="main">
-            <div class="shadow"></div>
-            
-            <div class="result">
-                <h2 class="title">Lorem Ipsum is simply</h2>
-                <div class="space-50"></div>
-                <Boxes />
+        <div className="container flex">
+          <div className="sidebar colored">
+            <h2 class="clock-title">text of the printing</h2>
+            <div className="clock-wrap">
+              <Clock />
             </div>
+            <img className='sidebar-float' src={PlaceHolder} alt="" />
+          </div>
+          <div className="main">
+              <div class="shadow"></div>
+              
+              <div class="result">
+                  <h2 class="title">Lorem Ipsum is simply</h2>
+                  <div class="space-50"></div>
+                  <Boxes />
+              </div>
+          </div>
         </div>
       </div>
     </>
